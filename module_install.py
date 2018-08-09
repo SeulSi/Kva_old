771fd092304e540f0047f6b0cbe19692222a1788
SeulSi <37247091+SeulSi@users.noreply.github.com>
2018-08-09 17:34:23 +0000

    Rename module_install to module_install.py

---
+++ module_install.py
@@ -0,0 +1,23 @@
+try:
+    import discord
+    import asyncio
+    import os
+    import sys
+    import time
+    from bs4 import BeautifulSoup
+    from collections import Collection
+    import info
+    from configparser import ConfigParser
+    import datetime
+    import json
+    import requests
+    import random
+    import tkinter
+except ImportError:
+    os.system("pip install -U discord-rewrite")
+    os.system("pip install asyncio")
+    os.system("pip install bs4")
+    os.system("pip install collections")
+    os.system("pip install requests")
+    os.system("pip install info")
+    os.system("pip install json")
